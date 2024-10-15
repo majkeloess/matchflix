912f21ea7007f29f287094c27104bb9c5885fde8
majkeloess <msaturczak@student.agh.edu.pl>
2024-10-15 22:52:38 +0200

    Added react-router-dom & created all pages and routing

--- src/test/unit/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "../../App";
-
-test("renders matchflix test", () => {
-  render(<App />);
-  const textElement = screen.getByText(/matchflix/i);
-  expect(textElement).toBeInTheDocument();
-});
