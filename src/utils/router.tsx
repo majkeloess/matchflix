8e2426e76e04a8300ce1e121309d64942b618eef
majkeloess <msaturczak@student.agh.edu.pl>
2024-10-17 14:04:35 +0200

    Router, ErrorPage implementation with tests

--- src/utils/router.tsx
+++ src/utils/router.tsx
@@ -1,20 +1,20 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
 import MoviesPage from "../pages/MoviesPage";
 import ListPage from "../pages/ListPage";
 import ErrorPage from "../pages/ErrorPage";
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
-    path: "/matchflix/",
+    path: "/",
     element: <LandingPage />,
   },
   {
-    path: "/matchflix/movies",
+    path: "/movies",
     element: <MoviesPage />,
     errorElement: <ErrorPage />,
   },
   {
-    path: "/matchflix/list",
+    path: "/list",
     element: <ListPage />,
     errorElement: <ErrorPage />,
   },
