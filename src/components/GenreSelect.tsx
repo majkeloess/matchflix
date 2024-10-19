4b77591efb32045732c0533e0260ec13a07e5b73
majkeloess <msaturczak@student.agh.edu.pl>
2024-10-19 15:31:39 +0200

    MoviePage tests

--- src/components/GenreSelect.tsx
+++ src/components/GenreSelect.tsx
@@ -6,7 +6,9 @@ function GenreSelect() {
       data-testid="select"
       className="border-4 border-pk rounded-xl text-pk text-xl md:text-2xl font-roboto font-regular min-w-32 bg-white/20"
     >
-      <option value="all">{TEXT.movieAll}</option>
+      <option id="all" value="all">
+        {TEXT.movieAll}
+      </option>
       {genres.map((genre, id) => (
         <option id={id.toString()} value={genre.toLowerCase()}>
           {genre}
