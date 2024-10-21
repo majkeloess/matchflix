d432e45612089fe8424b91677b3df48d5f277a16
majkeloess <msaturczak@student.agh.edu.pl>
2024-10-21 17:15:25 +0200

    UI Bug fix

--- src/utils/update.ts
+++ src/utils/update.ts
@@ -1,6 +1,6 @@
 export async function updateRecommendation(id: string, isAccepted: boolean) {
-  const url = `https://api.example/recommendations/${id}`;
-  const PUT = "PUT";
+  //const url = `https://api.example/recommendations/${id}`;
+  //const PUT = "PUT";
 
   // "Assume you have a backend", if it is real world scenario try/catch also
   if (isAccepted) {
