1103538f3cbfa1722f19c659c903f92c90c350cc
aloesss <msaturczak@student.agh.edu.pl>
2024-10-29 11:11:43 +0100

    Genre bug fix

--- src/hooks/useMovies.tsx
+++ src/hooks/useMovies.tsx
@@ -1,11 +1,15 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchMoviesRequest } from "../redux/actions";
+import { MoviesState } from "../constants/types";
 
 export const useMovies = () => {
   const dispatch = useDispatch();
+  const queryGenre = useSelector(
+    (selector: MoviesState) => selector.queryGenre
+  );
 
   useEffect(() => {
-    dispatch(fetchMoviesRequest("all"));
-  }, [dispatch]);
+    dispatch(fetchMoviesRequest(queryGenre));
+  }, [queryGenre]);
 };
